b349dc1efa67e14f79f725638be5e3073272e6ce
Rupinder Monga <rupindermonga@gmail.com>
2019-09-04 13:47:00 +0530

    Install and setup Redux

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,20 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Search from "./Search";
+import configureStore from "../configureStore";
+
+const store = configureStore();
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => "Home!"} />
-          <Route
-            exact
-            path="/search"
-            render={() => <Search latitude="2" longitude="4" address="ABCD" />}
-          />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => "Home!"} />
+            <Route
+              exact
+              path="/search"
+              render={() => (
+                <Search latitude="2" longitude="4" address="ABCD" />
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
